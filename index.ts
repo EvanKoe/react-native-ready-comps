965e22c3f5877cc7ac17b49d4c0c795585aa3425
EvanKoe <evan.koehler@epitech.eu>
2023-02-23 19:12:51 +0100

    [PATCH] how to waste another entire minor version

--- index.ts
+++ index.ts
@@ -15,11 +15,12 @@ import {
   ModalProps
 } from './constants/CompTypes';
 
-// import {AppRegistry} from 'react-native';
+import {AppRegistry} from 'react-native';
 // import App from './App';
 // import {name as appName} from './app.json';
 //
 // AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerRunnable("createRComps", createRComps);
 
 export type {
   RCompsType,
@@ -32,4 +33,5 @@ export type {
   TileProps,
   ModalProps
 };
+
 export default createRComps;
